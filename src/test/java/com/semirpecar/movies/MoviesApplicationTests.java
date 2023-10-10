9593f9970f6d34ea3ede0a20a7ec8b780e339c50
Semir Pecar <91013287+freemayne@users.noreply.github.com>
2023-10-10 13:10:23 +0200

    init backend

---
+++ src/test/java/com/semirpecar/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semirpecar.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
