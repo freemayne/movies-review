06b04d561bef7f19b1ee094e76bea59a15a4d4d4
Semir Pecar <91013287+freemayne@users.noreply.github.com>
2023-10-12 15:37:56 +0200

    api url changed to hosted

--- movieapp/src/api/fetchConfig.ts
+++ movieapp/src/api/fetchConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8080'; 
+const baseURL = 'https://service-semir-pecar-movie-review.onrender.com/'; 
 
 const api = axios.create({
   baseURL,
