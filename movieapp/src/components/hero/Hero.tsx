6a218e71f66110aacfa19b588f8cfe2e420b038e
Semir Pecar <91013287+freemayne@users.noreply.github.com>
2023-10-12 11:42:11 +0200

    added trailer route

--- movieapp/src/components/hero/Hero.tsx
+++ movieapp/src/components/hero/Hero.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
+import { Link } from "react-router-dom";
+import { FaPlayCircle } from "react-icons/fa";
 
 const Hero = ({ movies }: any) => {
   console.log(movies);
@@ -16,12 +18,30 @@ const Hero = ({ movies }: any) => {
                   backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,1)), url(${movie.backdrops[0]})`,
                 }}
               >
-                <div className="absolute top-[200px] flex justify-evenly w-full">
-                  <div className="h-[300px] overflow-hidden border border-solid border-yellow-400 rounded-xl">
-                    <img className="w-full h-full" src={movie.poster} alt="poster" />
+                <div className="absolute sm:top-[200px] flex justify-evenly w-full flex-col sm:flex-row items-center top-[20px]">
+                  <div className="h-[300px] overflow-hidden border border-solid border-yellow-500 rounded-xl">
+                    <img
+                      className="w-full h-full"
+                      src={movie.poster}
+                      alt="poster"
+                    />
                   </div>
                   <div className="text-white flex items-center">
                     <h4>{movie.title}</h4>
+                  </div>
+                  <div className="flex justify-between items-center width-[300px]">
+                    <Link
+                      to={`/Trailer/${movie.trailerLink.substring(
+                        movie.trailerLink.length - 11
+                      )}`}
+                    >
+                      <div className="p-0 m-0 w-[150px]">
+                      <FaPlayCircle className="play-button-icon p-0 m-0 text-yellow-500 text-5xl transform scale-100 transition-transform duration-300 hover:scale-125 cursor-pointer hover:text-white" />
+
+
+
+                      </div>
+                    </Link>
                   </div>
                 </div>
               </div>
---
+++ movieapp/src/components/trailer/Trailer.tsx
@@ -0,0 +1,18 @@
+import {useParams} from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import React from 'react'
+
+const Trailer = () => {
+
+    let params = useParams();
+    let key = params.ytTrailerId;
+
+  return (
+    <div className="h-[90vh]">
+      {(key!=null)?<ReactPlayer controls playing={true} url ={`https://www.youtube.com/watch?v=${key}`} 
+      width = '100%' height='100%' />:null}
+    </div>
+  )
+}
+
+export default Trailer
